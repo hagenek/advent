679b49a28a70712bf4ba54db262cc2a92b4c85fb
Georg Ekeberg <georg.ekeberg@sparebank1.no>
2023-12-08 16:39:31 +0100

    woek

---
+++ 2023/clj-solutions/src/day_two.clj
@@ -0,0 +1,8 @@
+(ns day-two
+  (:require [clojure.string :as s]))
+
+
+;; => "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+
+
+(def game1 (first (s/split-lines (slurp "resources/day2_part1_demo.txt"))))
