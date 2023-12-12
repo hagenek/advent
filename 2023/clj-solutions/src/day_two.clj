96a4fe58dd008e65e7381b9c08e39f0eebc199a1
Georg Ekeberg <georghagen@gmail.com>
2023-12-12 20:08:09 +0100

    done day2

--- 2023/clj-solutions/src/day_two.clj
+++ 2023/clj-solutions/src/day_two.clj
@@ -1,8 +1,61 @@
 (ns day-two
   (:require [clojure.string :as s]))
 
+(def demo-data (s/split-lines (slurp "resources/day2_demo.txt")))
+(def real-data (s/split-lines (slurp "resources/day2.txt")))
 
-;; => "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+(defn parse-game [game]
+  (map (fn [draw]
+         (reduce (fn [m [_ n color]]
+                   (assoc m (keyword color) (Integer/parseInt n)))
+                 {}
+                 (re-seq #"(\d+) (\w+)" draw)))
+       (s/split game #";")))
+
+(defn max-draws [game-data]
+  (reduce (fn [m draw]
+            (merge-with max m draw))
+          {}
+          game-data))
 
 
-(def game1 (first (s/split-lines (slurp "resources/day2_part1_demo.txt"))))
+
+(map max-draws (map parse-game demo-data))
+;; => ({:blue 6, :red 4, :green 2} {:blue 4, :green 3, :red 1} {:green 13, :blue 6, :red 20} {:green 3, :red 14, :blue 15} {:red 6, :blue 2, :green 3})
+
+(first (map max-draws (map parse-game demo-data)))
+;; => {:blue 6, :red 4, :green 2}
+
+(defn powerize [draw]
+  (* (:blue draw 0) (:red draw 0) (:green draw 0)))
+
+
+(defn possible-draw? [game-data]
+  (false? (or
+           (> (get game-data :red 0) 12)
+           (> (get game-data :green 0) 13)
+           (> (get game-data :blue 0) 14)
+           false)))
+
+(map possible-draw? (parse-game (nth real-data 1)))
+
+(defn possible-game? [game]
+  (every? identity (map possible-draw? (parse-game game))))
+
+(defn attach-id [bool-list]
+  (into {} (map-indexed (fn [id val]
+                          [(inc id) val]) bool-list)))
+
+(defn solv1 [game-data]
+  (let [possible-games (filter
+                        (fn [game] (second game)) (attach-id (map possible-game? game-data)))
+        possible-games-ids (map first possible-games)
+        id-sum (reduce + 0 possible-games-ids)]
+    id-sum))
+
+(solv1 real-data)
+
+;; => 2156
+
+
+
